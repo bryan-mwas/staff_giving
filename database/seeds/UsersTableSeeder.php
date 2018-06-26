75624e8e76f0077f54f9cbf459c22e976c0e953e
Brian Mwathi <mwathibrian7@gmail.com>
2018-06-26 23:58:12 +0300

    FEATURE: Added more users to seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,14 +12,37 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'name' => 'student',
+            'email' => 'student@app.com',
+            'role_id' => '1',
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
             'name' => 'John Doe',
             'email' => 'john@app.com',
+            'role_id' => '1',
             'password' => bcrypt('123456'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'Jane Doe',
             'email' => 'jane@app.com',
+            'role_id' => '1',
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'staff',
+            'email' => 'staff@app.com',
+            'role_id' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'committee',
+            'email' => 'committee@app.com',
+            'role_id' => '3',
             'password' => bcrypt('123456'),
         ]);
     }
